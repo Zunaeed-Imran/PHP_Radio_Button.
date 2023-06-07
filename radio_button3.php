69cd4c3c942073bcf95819b053fd29ecf12ef5d6
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-06-07 11:00:48 +0600

    Radio button use switch statement.

---
+++ radio_button3.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Radio Button</title>
+</head>
+<body>
+  <form action="radio_button.php" method="post">
+    <input type="radio" name="credit_card" value="Visa">Visa</br>
+    <input type="radio" name="credit_card" value="Master card">Master Card</br>
+    <input type="radio" name="credit_card" value="American express">American express</br>
+    <input type="submit" name="confirm" value="confirm">
+  </form>
+</body>
+</html>
+
+<?php
+if(isset($_POST["confirm"])){
+
+
+  $credit_card = null;
+
+  if(isset($_POST["credit_card"])){
+    $credit_card = $_POST["credit_card"];
+  }
+  switch($credit_card){
+    case "Visa":
+      echo "You selected Visa";
+      break;
+    case "Mastercard":
+      echo "You selected Master card";  
+      break;
+    case "American express":
+      echo "You selected American Express";
+      break;
+    default:
+      echo "Please make a selection";    
+  }
+}
+?>
