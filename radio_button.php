f2387386b13d69cb3b1fa69970ab8b474838db6b
imran <zunaeede@gmail.com>
2023-12-11 16:59:02 +0600

    main - add a note

--- radio_button.php
+++ radio_button.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Radio Button</title>
 </head>
+
 <body>
+  <!-- i use name becouse i only have to select one -->
   <form action="radio_button.php" method="post">
     <input type="radio" name="credit_card" value="caman_dude_what's_wrong">Visa</br>
     <input type="radio" name="credit_card" value="Master card">Master Card</br>
@@ -14,15 +17,16 @@
     <input type="submit" name="confirm" value="confirm">
   </form>
 </body>
+
 </html>
 
 <?php
-  if(isset($_POST["confirm"])){
-    if(isset($_POST["credit_card"])){
-      $credit_card = $_POST["credit_card"];
-      echo $credit_card;
-    }else{
-      echo "Please make a selection";
-    }
+if (isset($_POST["confirm"])) {
+  if (isset($_POST["credit_card"])) {
+    $credit_card = $_POST["credit_card"];
+    echo $credit_card;
+  } else {
+    echo "Please make a selection";
   }
+}
 ?>
